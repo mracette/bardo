90e0a9d1ae4329f13d3264695655e78658b374f9
mracette <markracette@gmail.com>
2022-07-31 18:40:23 -0500

    makeSprites utility

---
+++ src/util/makeSprites.ts
@@ -0,0 +1,28 @@
+import { Canvas2DGraphics, Canvas2DGraphicsRough, CanvasCoordinates } from 'crco-utils';
+
+export const makeSprites = (
+  graphics: Canvas2DGraphics | Canvas2DGraphicsRough,
+  spriteSize: number,
+  drawSprite: (graphics: Canvas2DGraphics | Canvas2DGraphicsRough) => void,
+  spriteCount = 4
+): HTMLCanvasElement[] => {
+  const sprites = [];
+  graphics.applyStyles(graphics.options.styles);
+  for (let i = 0; i < spriteCount; i++) {
+    const canvas = document.createElement('canvas');
+    const context = canvas.getContext('2d');
+    const spriteGraphics = new Canvas2DGraphicsRough(context, {
+      ...graphics.options,
+      styles: {
+        ...graphics.options.styles,
+        lineWidth: graphics.context.lineWidth
+      },
+      coords: new CanvasCoordinates({ baseWidth: spriteSize, baseHeight: spriteSize })
+    });
+    canvas.width = spriteSize;
+    canvas.height = spriteSize;
+    drawSprite(spriteGraphics);
+    sprites.push(canvas);
+  }
+  return sprites;
+};
